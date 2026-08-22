454d3014119e4359a01125550924cb06ea1f9e72
Álisson <romaosantosalisson@gmail.com>
2026-08-22 13:43:57 -0300

    :ambulance: Hotfix: Deploy and routes

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,7 @@ import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-<<<<<<< HEAD
-    <BrowserRouter basename="/x-streamers">
-=======
     <HashRouter>
->>>>>>> a76e3f7 (:ambulance: Hotfix: Deploy and routes)
       <App />
     </HashRouter>
   </StrictMode>,
